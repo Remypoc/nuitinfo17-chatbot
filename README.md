97e923b9ee0823ce10e20134ff47fbf92f43740b
vbouquet <valentin-bouquet@hotmail.fr>
2017-12-07 16:05:06 +0100

    Init repository with default bot

--- README.md
+++ README.md
@@ -1,8 +1,52 @@
-# nuitinfo17-chatbot
+# Dépot de la nuit de l'info 2017 - Chatbot
 
-L'équipe la plus classe du monde : 
+L'équipe la plus classe du monde:
 * Oussema Oulhaci
 * Valentin Bouquet
 * Rémy Pocquerusse
 
-Dépot de la nuit de l'info 2017 - Chatbot
+## Technologies
+
+* Npm: Javascript package manager
+* Webpack: Javascript module bundler
+* Babel: Transpiling code for new languages features
+* React: MVC Framework for dynamic statefull web pages
+
+## Prérequis
+* Git, gestionnaire de version - [Télécharger git](https://git-scm.com/downloads)
+* Virtualbox, outil de virtualisation - [Télécharger Virtualbox](https://www.virtualbox.org/wiki/Downloads)
+* Vagrant, gestionnaire de machine virtuelle - [Télécharger vagrant](https://www.vagrantup.com/downloads.html)
+
+## Installation
+
+```
+cd ~/{$project}
+vagrant up
+vagrant ssh
+cd /vagrant
+sudo mount --bind $HOME_DIR/vagrant_node_modules $PROJECT_DIR/node_modules
+npm run build
+node src/server/main.js &
+
+# Accéder au navigateur à l'URL http://localhost:8080
+```
+
+## CLI
+
+```
+# Lancer webpack avec la configuration pour produire le bundle.js (regroupement des dépendances javascript)
+# Option 1
+./node_modules/.bin/webpack -d --config webpack-config.js
+# Option 2
+npm run build
+# Option 3, depuis n'importe quel dossier
+npm run --prefix /vagrant build
+
+# Lancer webpack pour automatiser la génération à chaque modification
+# Attention: problème de synchronisation en dehors de la machine virtuelle
+
+# Option 1
+./node_modules/.bin/webpack -d --config webpack-config.js --watch
+# Option 2
+npm run watch
+```
---
+++ src/client/app/components/App.jsx
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ChatBot, { Loading } from 'react-simple-chatbot';
+
+// https://github.com/LucasBassetti/react-simple-chatbot
+
+class DBPedia extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      result: '',
+      trigger: false,
+    };
+
+    this.triggetNext = this.triggetNext.bind(this);
+  }
+
+  componentWillMount() {
+    const self = this;
+    const { steps } = this.props;
+    const search = steps.search.value;
+
+    const queryUrl = 'http://localhost:8080/test.json';
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('readystatechange', readyStateChange);
+
+    function readyStateChange() {
+      if (this.readyState === 4) {
+        const data = JSON.parse(this.responseText);
+        console.log(data);
+        if (data.robot && data.format == "simple_message") {
+          self.setState({ loading: false, result: data.message });
+        } else {
+          self.setState({ loading: false, result: 'Not found.' });
+        }
+      }
+    }
+
+    xhr.open('GET', queryUrl);
+    xhr.send();
+  }
+
+  triggetNext() {
+    this.setState({ trigger: true }, () => {
+      this.props.triggerNextStep();
+    });
+  }
+
+  render() {
+    const { trigger, loading, result } = this.state;
+
+    return (
+      <div style={{ width: '100%' }}>
+        { loading ? <Loading /> : result }
+        {
+          !loading &&
+          <div
+            style={{
+              textAlign: 'center',
+              marginTop: 20,
+            }}
+          >
+            {
+              !trigger &&
+              <button
+                onClick={() => this.triggetNext()}
+              >
+                Search Again
+              </button>
+            }
+          </div>
+        }
+      </div>
+    );
+  }
+}
+
+DBPedia.propTypes = {
+  steps: PropTypes.object,
+  triggerNextStep: PropTypes.func,
+};
+
+DBPedia.defaultProps = {
+  steps: undefined,
+  triggerNextStep: undefined,
+};
+
+const App = () => (
+  <ChatBot
+    steps={[
+      {
+        id: '1',
+        message: 'Salut, qui es-tu ?',
+        trigger: 'search',
+      },
+      {
+        id: 'search',
+        user: true,
+        trigger: '3',
+      },
+      {
+        id: '3',
+        component: <DBPedia />,
+        asMessage: true
+      },
+    ]}
+  />
+);
+
+export default App;
