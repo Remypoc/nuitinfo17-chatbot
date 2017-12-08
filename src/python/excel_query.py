b9c0020cfd78218494d171e2623730d84238d261
vbouquet <valentin-bouquet@hotmail.fr>
2017-12-08 01:42:38 +0100

    Fix bug with 'qui travaille sur le projet'

--- src/python/excel_query.py
+++ src/python/excel_query.py
@@ -14,7 +14,7 @@ def qui_travaille_sur_projet(projet):
     personnes_concernnees = []
     global personnes
     for person in personnes:
-        if person.projet == projet:
+        if person.PROJET == projet:
             personnes_concernnees.append(person)
     print(personnes_concernnees, file=sys.stderr)
     return personnes_concernnees
